5490525fd151e1c41187d927912c8b98a7f1aca1
zhangJingJie <zhangsalute1@gmail.com>
2023-12-17 17:42:22 +0800

    点击实现自增

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
+    <button @click="counterStore.increment">{{ counterStore.count }}</button>
   </div>
 </template>
 
 <script>
+import { usedefineStore } from "./stores/counter";
 export default {
   setup() {
+    const counterStore = usedefineStore();
+    console.log(counterStore);
     return {
+      counterStore,
     }
   }
 }
