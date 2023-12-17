355ea6dfc83d97e024785ee03729a46d5aa39aa8
zhangJingJie <zhangsalute1@gmail.com>
2023-12-17 16:56:48 +0800

    counter

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,3 +1,15 @@
 import { defineStore } from 'pinia' 
-export const useCounterStore = defineStore('counter', {
+import { ref } from 'vue'
+export const usedefineStore= defineStore('counter',()=>{
+    //定义数据
+    const count = ref(0)
+    //修改数据的方法(action 同步+异步)
+    const increment = ()=>{
+        count.value++
+    }
+    //以对象的方式return供组件使用
+    return {
+        count,
+        increment
+    }
 })
